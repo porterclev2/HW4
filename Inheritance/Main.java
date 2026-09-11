25a7bc92b297dce79900768e1a352fe411911207
Porter Clevidence <pbclevidence@cpp.edu>
2026-09-10 19:14:39 -0700

    modified driver to set Stephanie and Mary to hourly

--- Inheritance/Main.java
+++ Inheritance/Main.java
@@ -3,8 +3,8 @@ public class Main {
     public static void main(String[] args) {
         Employee[] employees = {
             new SalariedEmployee("Joe", "Jones", 111111111, 2500.00),
-            new Employee("Stephanie", "Smith", 222222222),
-            new Employee("Mary", "Quinn", 333333333),
+            new HourlyEmployee("Stephanie", "Smith", 222222222, 25.00, 32),
+            new HourlyEmployee("Mary", "Quinn", 333333333, 19.00, 47),
             new Employee("Nicole", "Dior", 444444444),
             new SalariedEmployee("Renwa", "Chanel", 555555555, 1700.00),
             new Employee("Mike", "Davenport", 666666666),
