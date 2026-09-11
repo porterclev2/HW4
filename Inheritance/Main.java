3bbdb8e00f1e097c6e83b42fc05bd79950ee2f29
Porter Clevidence <pbclevidence@cpp.edu>
2026-09-10 19:06:32 -0700

    moved main to folder of part 1

--- Inheritance/Main.java
+++ Inheritance/Main.java
@@ -2,17 +2,17 @@ package Inheritance;
 public class Main {
     public static void main(String[] args) {
         Employee[] employees = {
-            new Employee("Joe", "Jones", 111111111),
+            new SalariedEmployee("Joe", "Jones", 111111111, 2500.00),
             new Employee("Stephanie", "Smith", 222222222),
             new Employee("Mary", "Quinn", 333333333),
             new Employee("Nicole", "Dior", 444444444),
-            new Employee("Renwa", "Chanel", 555555555),
+            new SalariedEmployee("Renwa", "Chanel", 555555555, 1700.00),
             new Employee("Mike", "Davenport", 666666666),
             new Employee("Mahnaz", "Vaziri", 777777777)
         };
 
         for (Employee emp : employees) {
-            System.out.println(emp.getFirstName() + " " + emp.getLastName() + " " + emp.getSocialSecurityNumber());
+            System.out.println(emp);
         }
     }
 }
