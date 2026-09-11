b69486830cf002eaf5bdd6d2f24be500fc633205
Porter Clevidence <pbclevidence@cpp.edu>
2026-09-10 19:26:23 -0700

    changed Nicole and Mahnaz to commission employees, changed prev double values to ints

--- Inheritance/Main.java
+++ Inheritance/Main.java
@@ -2,13 +2,13 @@ package Inheritance;
 public class Main {
     public static void main(String[] args) {
         Employee[] employees = {
-            new SalariedEmployee("Joe", "Jones", 111111111, 2500.00),
-            new HourlyEmployee("Stephanie", "Smith", 222222222, 25.00, 32),
-            new HourlyEmployee("Mary", "Quinn", 333333333, 19.00, 47),
-            new Employee("Nicole", "Dior", 444444444),
-            new SalariedEmployee("Renwa", "Chanel", 555555555, 1700.00),
+            new SalariedEmployee("Joe", "Jones", 111111111, 2500),
+            new HourlyEmployee("Stephanie", "Smith", 222222222, 25, 32),
+            new HourlyEmployee("Mary", "Quinn", 333333333, 19, 47),
+            new CommissionEmployee("Nicole", "Dior", 444444444, 0.15, 50000),
+            new SalariedEmployee("Renwa", "Chanel", 555555555, 1700),
             new Employee("Mike", "Davenport", 666666666),
-            new Employee("Mahnaz", "Vaziri", 777777777)
+            new CommissionEmployee("Mahnaz", "Vaziri", 777777777, 0.22, 40000)
         };
 
         for (Employee emp : employees) {
