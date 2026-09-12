07a30f2194b3eddb3326d9964582fce29ed9f513
Porter Clevidence <pbclevidence@cpp.edu>
2026-09-11 19:22:50 -0700

    driver code version 1

--- Aggregation/Main.java
+++ Aggregation/Main.java
@@ -1,5 +1,16 @@
 package Aggregation;
 
 public class Main {
-    
+    public static void main(String[] args) {
+        Course course = new Course("Best Course Ever", 
+                                   "John", 
+                                   "Pork", 
+                                   "3-2636", 
+                                   "Clean Code", 
+                                   "George Orwell", 
+                                   "ACME"
+                                );
+
+        course.print();
+    }
 }
