c8a1e907254b09a00917bd3c45926875fa828786
Porter Clevidence <pbclevidence@cpp.edu>
2026-09-11 19:04:01 -0700

    driver code w/ship, cruiseship, and cargoship

--- Polymorphism/Main.java
+++ Polymorphism/Main.java
@@ -1,5 +1,17 @@
 package Polymorphism;
 
 public class Main {
+
+    static Ship[] ships = new Ship[3];
     
+    public static void main(String[] args) {
+
+        ships[0] = new Ship("S.S. Jade", "1999");
+        ships[1] = new CruiseShip("Nevermore", "2020", 20);
+        ships[2] = new CargoShip("S.S. Heavy LD", "2026", 2000);
+
+        for (Ship s : ships) {
+            s.print();
+        }
+    }
 }
