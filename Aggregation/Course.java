2734c92ea317aa5c0db99cbb3b2bd1d74a233e72
Porter Clevidence <pbclevidence@cpp.edu>
2026-09-11 19:33:54 -0700

    changed course to allow 2 instructors and 2 textbooks, changed driver code

--- Aggregation/Course.java
+++ Aggregation/Course.java
@@ -2,13 +2,21 @@ package Aggregation;
 
 public class Course {
     private String name;
-    private Instructor instructor;
-    private Textbook textbook;
+    private Instructor instructor1;
+    private Instructor instructor2;
+    private Textbook textbook1;
+    private Textbook textbook2;
 
-    public Course(String n, String inst_first, String inst_last, String inst_office, String book_title, String book_author, String book_publisher) {
+    public Course(String n, String inst1_first, String inst1_last, String inst1_office, 
+                    String inst2_first, String inst2_last, String inst2_office, 
+                    String book1_title, String book1_author, String book1_publisher, 
+                    String book2_title, String book2_author, String book2_publisher) {
         this.name = n;
-        this.instructor = new Instructor(inst_first, inst_last, inst_office);
-        this.textbook = new Textbook(book_title, book_author, book_publisher);
+        this.instructor1 = new Instructor(inst1_first, inst1_last, inst1_office);
+        this.textbook1 = new Textbook(book1_title, book1_author, book1_publisher);
+
+        this.instructor2 = new Instructor(inst2_first, inst2_last, inst2_office);
+        this.textbook2 = new Textbook(book2_title, book2_author, book2_publisher);
     }
 
     public void setName(String n) {
@@ -19,17 +27,27 @@ public class Course {
         return this.name;
     }
 
-    public Instructor getInstructor() {
-        return this.instructor;
+    public Instructor getInstructor1() {
+        return this.instructor1;
     }
 
-    public Textbook getTextbook() {
-        return this.textbook;
+    public Instructor getInstructor2() {
+        return this.instructor2;
+    }
+
+    public Textbook getTextbook1() {
+        return this.textbook1;
+    }
+
+    public Textbook getTextbook2() {
+        return this.textbook2;
     }
 
     public void print() {
         System.out.printf("Course: %s\n", this.name);
-        System.out.printf("Instructor: %s %s\n", this.instructor.getFirstName(), this.instructor.getLastName());
-        System.out.printf("Textbook: %s by %s\n", this.textbook.getTitle(), this.textbook.getAuthor());
+        System.out.printf("Instructor: %s %s; Textbook: %s by %s\n", this.instructor1.getFirstName(), 
+                            this.instructor1.getLastName(), this.textbook1.getTitle(), this.textbook1.getAuthor());
+        System.out.printf("Instructor: %s %s; Textbook: %s by %s\n", this.instructor2.getFirstName(), 
+                            this.instructor2.getLastName(), this.textbook2.getTitle(), this.textbook2.getAuthor());
     }
 }
--- Aggregation/Main.java
+++ Aggregation/Main.java
@@ -2,13 +2,19 @@ package Aggregation;
 
 public class Main {
     public static void main(String[] args) {
-        Course course = new Course("Best Course Ever", 
-                                   "John", 
-                                   "Pork", 
-                                   "3-2636", 
-                                   "Clean Code", 
-                                   "George Orwell", 
-                                   "ACME"
+        Course course = new Course("CS 5800 Advanced Software Engineering",
+                                   "Nima",
+                                   "Davarpanah",
+                                   "3-2636",
+                                   "John",
+                                   "Pork",
+                                   "3-2637",
+                                   "Clean Code",
+                                   "Robert C. Martin",
+                                   "Prentice Hall",
+                                   "Dirty Code",
+                                   "Mobert C. Rartin",
+                                   "Professional Room"
                                 );
 
         course.print();
